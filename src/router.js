613b27eda2cb954457723cbd99a3bb7fd6bdfeda
Jeffrey Walker <jeffrey.walker@noaa.gov>
2026-04-29 14:49:00 -0400

    feat: change router to hash mode

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const RoutePlaceholder = {
   render: () => null
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
